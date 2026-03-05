b580720ad11f1c1ada31c8f03f6fdad3b1f46aba
morochrislegal <christian.moro@escola.pr.gov.br>
2026-03-05 14:12:10 +0000

    listadeexercicios

---
+++ lista3.js
@@ -0,0 +1,35 @@
+//criar um programa para verificar se o número 12 é divisivel por 2 e por 3
+let num = 12;
+
+if (num % 2 === 0 && num % 3 === 0) {
+    console.log(`o número ${num} é divisível por 2 e por 3`);
+} else {
+    console.log(`o número ${num} não é divisível por 2 e por 3`);
+}
+//criar um programa para mostrar um desconto de 12% para produtos acima de 80$
+let preco = 800;    
+let desconto = preco * 0.88
+
+if (preco >= 800) { 
+    console.log(`o preço com desconto é ${desconto}`);}
+    else {
+        console.log(`não tem desconto`);
+    }
+//criar um programa para mostrar apenas os números pares entre 1 e 20 
+for(let i = 1; i <= 10 ; i++) {
+    if (i % 2 === 0) {
+        console.log(`o número ${i} é par`);
+    }
+}
+//criar um programa para mostrar apenas os número ímpares entre 1 e 20
+for(let i = 1; i <= 20 ; i++) {
+    if (i % 2 !== 0) {
+        console.log(`o número ${i} é ímpar`);
+    }
+}
+//criar um progama para mostrar a tabuada do 5 
+//criar um programa para mostrar os números de 1 a 7 usando while
+//criar um programa para mostrar os a tabuada do 3 usando while 
+//criar um programa para mostrar o fatorial de 5
+//criar um programa para saber se o número 7 é primario
+//criar um programa para calcular a potencia de 2 elevado a 3
